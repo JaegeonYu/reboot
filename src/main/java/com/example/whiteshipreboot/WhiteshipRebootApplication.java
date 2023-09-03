48c8a26397eff36632e20d128a2e442854d54485
유재건 <yjk9805@naver.com>
2023-09-03 22:30:21 +0900

    chore: initialize

---
+++ src/main/java/com/example/whiteshipreboot/WhiteshipRebootApplication.java
@@ -0,0 +1,13 @@
+package com.example.whiteshipreboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteshipRebootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteshipRebootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whiteshipreboot/WhiteshipRebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whiteshipreboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteshipRebootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
